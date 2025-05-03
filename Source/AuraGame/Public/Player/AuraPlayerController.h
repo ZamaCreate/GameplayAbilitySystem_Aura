737798e8374a58e5f15e3c225c6b1648d1aacf43
DevNoctivagant <zak.marchetti@gmail.com>
2025-05-03 15:28:08 -0400

    9. Enhanced Input
    UE5 and VS

---
+++ Source/AuraGame/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright DevNoctivagant
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAME_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
